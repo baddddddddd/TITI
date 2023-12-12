94d6ba70184fea0a36cad6bebcf87a9563b741f6
Eluxe <eluxeofficial@gmail.com>
2023-12-12 12:18:11 +0800

    Prepare for merging with main branch

--- routers/course.js
+++ routers/course.js
@@ -2,23 +2,89 @@ const express = require("express");
 const db = require("../db");
 const router = express.Router();
 
+const assignmentRouter = require("./assignment");
+router.use("/assignment", assignmentRouter);
+
 
 router.get("/", (req, res) => {
     // View Courses
-    res.render("../views/course");
+    const userId = req.session.userId;
+    
+    const query = "SELECT CourseID, CourseCode, ClassName, Section, FirstName, MiddleName, LastName FROM Courses JOIN UserProfile ON Courses.InstructorID = UserProfile.UserID NATURAL JOIN CourseEnrolees WHERE StudentID=?";
+    const params = [userId];
 
+    db.query(query, params, (err, result) => {
+        let courses = [];
+        
+        result.forEach((course) => {
+            courses.push({
+                courseCode: course.CourseCode,
+                courseName: course.ClassName,
+                section: course.Section,
+                instructor: `${course.LastName}, ${course.FirstName} ${course.MiddleName}`,
+            });
+        });
+
+        res.render("course", { heading: "Enrolled Courses", courses: courses });
+    });
+});
+
+router.get("/owned", (req, res) => {
+    // View Courses
+    const userId = req.session.userId;
+    
+    const query = "SELECT * FROM Courses JOIN UserProfile ON Courses.InstructorID = UserProfile.UserID WHERE InstructorID=?";
+    const params = [userId];
+
+    db.query(query, params, (err, result) => {
+        let courses = [];
+        
+        result.forEach((course) => {
+            courses.push({
+                courseCode: course.CourseCode,
+                courseName: course.ClassName,
+                section: course.Section,
+                instructor: `${course.LastName}, ${course.FirstName} ${course.MiddleName}`,
+            });
+        });
+
+        res.render("course", { heading: "Owned Courses", courses: courses });
+    });
+});
+
+router.get("/owned", (req, res) => {
+    // View Courses
+    const userId = req.session.userId;
+    
+    const query = "SELECT * FROM Courses JOIN UserProfile ON Courses.InstructorID = UserProfile.UserID WHERE InstructorID=?";
+    const params = [userId];
+
+    db.query(query, params, (err, result) => {
+        let courses = [];
+        
+        result.forEach((course) => {
+            courses.push({
+                courseCode: course.CourseCode,
+                courseName: course.ClassName,
+                section: course.Section,
+                instructor: `${course.LastName}, ${course.FirstName} ${course.MiddleName}`,
+            });
+        });
+
+        res.render("course", { courses: courses });
+    });
 });
 
 router.get("/create", (req, res) => {
     // Create courses, for teachers only
-    res.render("../views/course/create");
+    res.render("course/create");
 });
 
 
 
 router.get("/join", (req, res) => {
     // Join courses, for students only
-    res.send("Join course");
+    res.render("course/join");
 });
 
 router.get("/:courseId", (req, res) => {
@@ -37,6 +103,10 @@ router.get("/:courseId", (req, res) => {
         let subject = course.Subject;
         let section = course.Section;
         let room = course.Room;
+        let instructorId = course.InstructorID;
+        let courseId = course.CourseID;
+
+        let isInstructor = instructorId == req.session.userId;
 
         db.query("SELECT CourseID FROM Courses WHERE CourseCode=?", [req.params.courseId], (err, result) => {
             let courseID = result[0].CourseID;
@@ -45,16 +115,55 @@ router.get("/:courseId", (req, res) => {
             let params = [courseID];
 
             db.query(query, params, (err, result) => {
-                let stream = [];
+                let materials = [];
 
                 result.forEach((item) => {
-                    stream.push({
+                    materials.push({
+                        type: "material",
                         name: item.FileName,
                         path: item.FilePath,
+                        date: item.UploadDate,
                     });
                 });
 
-                res.render("../views/course", { name: name, subject: subject, section: section, room: room, code: req.params.courseId, stream: stream });
+                const query = "SELECT FirstName, MiddleName, LastName, Content, AnnouncementDate FROM Announcements JOIN UserProfile ON AnnouncerID = UserID WHERE CourseID=?";
+                const params = [courseID];
+
+                db.query(query, params, (err, result) => {
+                    let announcements = [];
+
+                    result.forEach((announcement) => {
+                        console.log(announcement.AnnouncementDate);
+                        announcements.push({
+                            type: "announcement",
+                            announcer: `${announcement.LastName}, ${announcement.FirstName} ${announcement.MiddleName}`,
+                            date: announcement.AnnouncementDate,
+                            content: announcement.Content,
+                        });
+                    });
+
+                    const query = "SELECT * FROM Assignments WHERE CourseID=?";
+                    const params = [courseID];
+
+                    db.query(query, params, (err, result) => {
+                        let assignments = [];
+
+                        result.forEach((assignment) => {
+                            assignments.push({
+                                type: "activity",
+                                title: assignment.AssignmentTitle,
+                                date: assignment.UploadDate,
+                                dueDate: assignment.DueDate,
+                                id: assignment.AssignmentID,
+                            });
+                        }); 
+
+                        let streams = [...announcements, ...materials, ...assignments];
+                        streams.sort((a, b) => b.date - a.date);
+    
+                        res.render("course/overview", { courseId: courseId, isInstructor: isInstructor, name: name, subject: subject, section: section, room: room, code: req.params.courseId, stream: streams });    
+                    })
+                });
             });
         });
     });
